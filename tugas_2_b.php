b601667ddfa5be9e09f2dec28ac2ae2b0f5e0fce
thaerachman <71365565+thaerachman@users.noreply.github.com>
2020-10-07 15:37:27 +0700

    Add files via upload

---
+++ tugas_2_b.php
@@ -0,0 +1,16 @@
+<?php
+echo "Tugas 2b php <br>";
+echo "------------------ <br>";
+$njual=80000;
+$nbeli=50000;
+
+// Rumus:
+$untung=($nbeli / $njual) * 100;
+
+echo "Harga Jual : ".$njual;
+echo "<br>";
+echo "Harga Beli : ".$nbeli;
+echo "<br>";
+echo "Keuntungan : ".$untung."%";
+
+?>
